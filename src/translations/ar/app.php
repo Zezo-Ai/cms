c2bb894fa2c16fde8320223e3c4c1f5dd61ae447
shinybrad <83238715+shinybrad@users.noreply.github.com>
2025-01-19 12:16:25 -0500

    New translations app.php (Arabic) [ci skip]

--- src/translations/ar/app.php
+++ src/translations/ar/app.php
@@ -1718,6 +1718,7 @@ return [
     'This can begin with an environment variable.' => 'يمكن أن يبدأ هذا بمتغير لبيئة العمل.',
     'This draft’s entry type is no longer available. You can still view it, but not apply it.' => 'لم يعُد نوع الإدخال في هذه المسودة متوفرًا. ما زال بإمكانك عرضها، ولكن لا يمكنك تطبيقها.',
     'This element is conditional' => 'هذا العنصر شرطي',
+    'This entry type’s {attributes} {totalAttributes, plural, =1{has} other{have}} been overridden.' => 'This entry type’s {attributes} {totalAttributes, plural, =1{has} other{have}} been overridden.',
     'This field has a tip' => 'This field has a tip',
     'This field has a warning' => 'This field has a warning',
     'This field has been modified.' => 'تم تعديل هذا الحقل.',
