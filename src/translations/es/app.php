1618c4305d1997eb327e3e23d397f4d5851a4d16
shinybrad <83238715+shinybrad@users.noreply.github.com>
2025-01-19 12:16:24 -0500

    New translations app.php (Spanish) [ci skip]

--- src/translations/es/app.php
+++ src/translations/es/app.php
@@ -1718,6 +1718,7 @@ return [
     'This can begin with an environment variable.' => 'Puede comenzar con una variable de entorno.',
     'This draft’s entry type is no longer available. You can still view it, but not apply it.' => 'El tipo de entrada de este borrador ya no está disponible. Puedes verlo, pero no aplicarlo.',
     'This element is conditional' => 'Este elemento es condicional',
+    'This entry type’s {attributes} {totalAttributes, plural, =1{has} other{have}} been overridden.' => 'This entry type’s {attributes} {totalAttributes, plural, =1{has} other{have}} been overridden.',
     'This field has a tip' => 'This field has a tip',
     'This field has a warning' => 'This field has a warning',
     'This field has been modified.' => 'Se ha modificado este campo.',
