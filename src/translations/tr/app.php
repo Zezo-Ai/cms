ac3be2152729ace7811b5b35dfc650046f497d1c
shinybrad <83238715+shinybrad@users.noreply.github.com>
2025-01-19 12:16:46 -0500

    New translations app.php (Turkish) [ci skip]

--- src/translations/tr/app.php
+++ src/translations/tr/app.php
@@ -1718,6 +1718,7 @@ return [
     'This can begin with an environment variable.' => 'Bu, bir ortam değişkeni ile başlayabilir.',
     'This draft’s entry type is no longer available. You can still view it, but not apply it.' => 'Bu taslağın kayıt türü artık kullanılamıyor. Onu hâlâ görüntüleyebilirsiniz ama uygulayamazsınız.',
     'This element is conditional' => 'Bu öge koşullu',
+    'This entry type’s {attributes} {totalAttributes, plural, =1{has} other{have}} been overridden.' => 'This entry type’s {attributes} {totalAttributes, plural, =1{has} other{have}} been overridden.',
     'This field has a tip' => 'This field has a tip',
     'This field has a warning' => 'This field has a warning',
     'This field has been modified.' => 'Bu alan değiştirildi.',
