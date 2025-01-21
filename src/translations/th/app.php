82f3d5cf69674a88b747bfe2704547431fcb5d85
shinybrad <83238715+shinybrad@users.noreply.github.com>
2025-01-21 07:30:22 -0500

    New translations app.php (Thai) [ci skip]

--- src/translations/th/app.php
+++ src/translations/th/app.php
@@ -1314,7 +1314,7 @@ return [
     'Regenerating project config YAML files from the loaded project config…' => 'สร้างไฟล์การกำหนดค่าโครงการ YAML ของคุณใหม่จากการกำหนดค่าโครงการที่โหลดแล้ว...',
     'Region' => 'ภูมิภาค',
     'Register users' => 'ลงทะเบียนผู้ใช้',
-    'Relate {type} from a specific site?' => 'Relate {type} from a specific site?',
+    'Relate {type} from a specific site?' => 'เชื่อมโยง {type} จากเว็บไซต์เฉพาะหรือไม่',
     'Related To' => 'เกี่ยวข้องกับ',
     'Related {type} Title' => 'ชื่อ {type} ที่เกี่ยวข้อง',
     'Relation ({ex})' => 'Relation ({ex})',
