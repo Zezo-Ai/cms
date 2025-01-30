fe4ab0704ff220033141fd1a825729df932d5bf2
brandonkelly <brandon@pixelandtonic.com>
2025-01-30 15:48:29 -0800

    Cleanup

--- src/web/assets/cp/src/js/LinkField.js
+++ src/web/assets/cp/src/js/LinkField.js
@@ -21,12 +21,12 @@ Craft.LinkField = Garnish.Base.extend({
       .find('.text:first');
 
     if (this.$typeSelect.length) {
-      if (!this.$typeSelect.data('fieldtoggle')) {
-        this.$typeSelect.fieldtoggle();
-      }
-      this.$typeSelect.data('fieldtoggle').on('toggleChange', () => {
-        this.updateLabel();
-      });
+      this.$typeSelect
+        .fieldtoggle()
+        .data('fieldtoggle')
+        .on('toggleChange', () => {
+          this.updateLabel();
+        });
     }
 
     this.addListener(this.$container, 'labelChanged', () => {
