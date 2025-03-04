5e34c58bc5ca2e3674d3e99303b1666a31d805e8
brandonkelly <brandon@pixelandtonic.com>
2025-03-04 14:22:28 -0800

    Let the field type decide if the value is empty

--- src/services/Elements.php
+++ src/services/Elements.php
@@ -3741,14 +3741,15 @@ class Elements extends Component
                     if ($fieldLayout) {
                         foreach ($fieldLayout->getCustomFields() as $field) {
                             if (($saveContent || in_array($field->handle, $dirtyFields)) && $field::dbType() !== null) {
-                                $serializedValue = $field->serializeValue($element->getFieldValue($field->handle), $element);
+                                $value = $element->getFieldValue($field->handle);
+                                if ($element->isNewForSite && $field->isValueEmpty($value, $element)) {
+                                    // don't store empty values if element is new for site
+                                    // https://github.com/craftcms/cms/issues/16797
+                                    continue;
+                                }
+                                $serializedValue = $field->serializeValue($value, $element);
                                 if ($serializedValue !== null) {
-                                    if ($element->isNewForSite && empty($serializedValue)) {
-                                        // don't store empty values if element is new for site
-                                        // https://github.com/craftcms/cms/issues/16797
-                                    } else {
-                                        $content[$field->layoutElement->uid] = $serializedValue;
-                                    }
+                                    $content[$field->layoutElement->uid] = $serializedValue;
                                 } elseif (!$saveContent) {
                                     // if serialized value is null, and we're not saving all the content,
                                     // we need to register the fact that the new value is empty
